1f922390669dfe88c7d2c036448140b95dd40960
Youngwoo <youngwoo@example.com>
2026-01-20 17:19:04 +0900

    파비콘2

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "KR-600 프리미엄 무전기",
   description: "호텔, 웨딩홀, 대형 식당을 위한 전문 무전기",
+  icons: {
+    icon: '/logo.png',
+  },
 };
 
 export default function RootLayout({
